834eabf65716ed8170ffb69208e0643683d9fe92
Nick Barba <nsbarba1@gmail.com>
2025-01-24 15:30:30 -0700

    hm

--- Main.R
+++ Main.R
@@ -1,9 +1,9 @@
-### Aquatic Forecast Workflow ##
-devtools::install_github('eco4cast/neon4cast', force = TRUE)
-install.packages('rMR')
+### Aquatic Forecast Workflow ###
+# devtools::install_github("eco4cast/neon4cast")
 library(tidyverse)
 library(neon4cast)
 library(lubridate)
+#install.packages("rMR")
 library(rMR)
 
 forecast_date <- Sys.Date()
